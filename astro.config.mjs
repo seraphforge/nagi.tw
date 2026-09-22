e4451798e4f67257e5afa1c9198c811c9d560eba
Casper <casp3790er@gmail.com>
2026-09-23 01:19:47 +0800

    config: use GitHub Pages domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://nagi.tw',
+  site: 'https://seraphforge.github.io',
+  base: '/nagi.tw',
   output: 'static',
   integrations: [sitemap()],
 });
